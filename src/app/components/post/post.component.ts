d439946aa1c02326d355be8183be2f44d7f2654e
Mike <Mikhail_Yermalaeu@epam.com>
2017-09-20 13:29:53 +0300

    add some tips

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -10,7 +10,7 @@ import { PostsService } from '../../services';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['post.component.css']
 })
 export class PostComponent implements OnInit {
 	comments: Comment[] = [];
